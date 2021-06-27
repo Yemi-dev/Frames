ab6c8195f8e2c7eaa058f1a247fcbb6c363454ae
Ad3mola <ademolamadelewi@gmail.com>
2021-06-27 01:44:29 +0100

    finish setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Sample from 'pages/Sample';
 import React from 'react';
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div>
+      <Sample title="SampleComponent" description="This is the sample component description" />
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
 
+import { GlobalStyle } from '../src/styles/global';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
